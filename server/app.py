34e1d71223804c8b6819cf5ce54c41d5281abbf8
Romil <romil.m.kothari@gmail.com>
2024-02-09 12:59:11 -0500

    almost done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,16 +8,79 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        username = request.get_json().get('username')
+        password = request.get_json().get('password')
+        image_url = request.get_json().get('image_url')
+        bio = request.get_json().get('bio')
+
+        if not username or not password:
+            return {'error': 'Username and password are required'}, 422
+
+        try:
+            user = User(username=username, image_url=image_url, bio=bio)
+            user.password_hash = password
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+
+            response =  {
+                'user_id': user.id,
+                'username': user.username,
+                'image_url': user.image_url,
+                'bio': user.bio
+            }, 201
+            return response
+        
+
+        except IntegrityError:
+            # Handle any integrity constraint violations (e.g., duplicate username)
+            db.session.rollback()
+            return {'error': 'Username already exists'}, 422
+
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            response =  {
+                'id': user.id,
+                'username': user.username,
+                'image_url': user.image_url,
+                'bio': user.bio
+            }, 200
+            return response
+        else:
+            return {}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json().get('username')
+        ).first()
+
+        if user and user.authenticate(request.get_json().get('password')) == True:
+            session['user_id'] = user.id
+            response =  {
+                'id': user.id,
+                'username': user.username,
+                'image_url': user.image_url,
+                'bio': user.bio
+            }, 200
+            return response
+        else:
+            return {}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            session['user_id'] = None
+            return {}, 204
+        else:
+            return {}, 401
 
 class RecipeIndex(Resource):
     pass
